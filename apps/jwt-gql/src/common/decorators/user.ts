ef95228edbf21ee040ea1bc29f7705d7f0441a19
trejgun <trejgun@gmail.com>
2021-04-21 11:18:09 +0800

    mark unused param as _

--- apps/jwt-gql/src/common/decorators/user.ts
+++ apps/jwt-gql/src/common/decorators/user.ts
@@ -3,7 +3,7 @@ import {GqlExecutionContext} from "@nestjs/graphql";
 
 import {IUser} from "../../user/interfaces";
 
-export const User = createParamDecorator((data: unknown, context: ExecutionContext) => {
+export const User = createParamDecorator((_data: unknown, context: ExecutionContext) => {
   const ctx = GqlExecutionContext.create(context);
   return (ctx.getContext().req.user as IUser) || null;
 });
