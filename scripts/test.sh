6aeb10d9594ed73ebd427731fa9847be7ebfa6cc
Yury Toporov <gemuniongroup@gmail.com>
2021-08-27 12:41:53 +0800

    fix: add postgres env for test

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,8 @@ echo -e "\033[34mTesting...\n\033[0m";
 
 set -e # this will cause the shell to exit immediately if any command exits with a nonzero exit value.
 
-export NODE_ENV=test
+export NODE_ENV=$NODE_ENV
+export POSTGRES_URL=$POSTGRES_URL
 
 lerna bootstrap --hoist
 
