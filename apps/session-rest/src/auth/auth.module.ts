eefb6c37a03784de376b523c3e00b0795250c620
Trej Gun <185285+TrejGun@users.noreply.github.com>
2022-06-18 07:44:44 +0800

    Update passport (#203)
    * fix(deps): update all non-major dependencies
    
    * feat: manually update passport
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- apps/session-rest/src/auth/auth.module.ts
+++ apps/session-rest/src/auth/auth.module.ts
@@ -7,10 +7,12 @@ import { SessionSerializer } from "./session.serializer";
 import { AuthSessionController } from "./auth.session.controller";
 import { AuthSocialController } from "./auth.social.controller";
 import { FacebookStrategy, GoogleStrategy, LocalStrategy, OneloginStrategy } from "./strategies";
+import { AuthService } from "./auth.service";
 
 @Module({
   imports: [UserModule, PassportModule, ConfigModule],
-  providers: [FacebookStrategy, GoogleStrategy, LocalStrategy, OneloginStrategy, SessionSerializer],
+  providers: [FacebookStrategy, GoogleStrategy, LocalStrategy, OneloginStrategy, SessionSerializer, AuthService],
   controllers: [AuthSessionController, AuthSocialController],
+  exports: [AuthService],
 })
 export class AuthModule {}
